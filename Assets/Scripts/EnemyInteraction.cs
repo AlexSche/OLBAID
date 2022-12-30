81dcd484a76ec7cff2524b7ecc5451032fdc5ec2
Alex <Alexander.S_97@gmx.de>
2022-12-30 12:46:58 +0700

    enemy plays dead animation and gets removed after delay

--- Assets/Scripts/EnemyInteraction.cs
+++ Assets/Scripts/EnemyInteraction.cs
@@ -6,12 +6,16 @@ public class EnemyInteraction : MonoBehaviour
 {
     private int maxHealth;
     private int currentHealth;
+    public Animator animator;
+    private EnemyMovement enemyMovement;
     public HealthbarScript healthbar;
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = 100;
         currentHealth = maxHealth;
+        //animator = GetComponent<Animator>();
+        enemyMovement = GetComponent<EnemyMovement>();
     }
 
     // Update is called once per frame
@@ -25,10 +29,17 @@ public class EnemyInteraction : MonoBehaviour
         healthbar.setHealth(currentHealth);
         if (currentHealth <= 0) {
             die();
+            enemyMovement.enabled = false;
+            GetComponent<CharacterController>().enabled = false;
         }
     }
 
-    private void die () {
-        Debug.Log("Enemy died!");
+    private void die() {
+        animator.SetBool("isDead", true);
+    }
+
+    public void removeEnemy() {
+        //remove Enemy after 3 seconds delay
+        Destroy(this.gameObject, 3);
     }
 }
