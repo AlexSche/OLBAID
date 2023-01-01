3a890d550b2474009fcefa49532f41b18470c4a8
Alex <Alexander.S_97@gmx.de>
2023-01-01 14:01:51 +0700

    edited movement script and rotation works

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed;
-    public CharacterController characterController;
+    public float movementSpeed = 2f;
+    public Tilemap map;
+    private MouseInput mouseInput;
     private Vector3 targetPosition;
     private Animator animator;
     private PlayerAttack playerAttack;
+
+    private void Awake() => mouseInput = new MouseInput();
+    private void OnEnable() => mouseInput.Enable();
+    private void OnDisable() => mouseInput.Disable();
     void Start()
-    {
+    {        
         animator = GetComponent<Animator>();
         playerAttack = GetComponent<PlayerAttack>();
+        targetPosition = transform.position;
+        mouseInput.Mouse.MouseClick.performed += context => mouseClick(context);
     }
 
     // Update is called once per frame
@@ -20,58 +29,46 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            locateMousePositionOnTerrain();
-        }
-        else if (Input.GetKey(KeyCode.Mouse1))
-        {
             playerAttack.useBasicAttack();
         }
         else if (Input.GetKey(KeyCode.Alpha1)) {
             playerAttack.useWhipWhirl();
         }
-        moveToPosition();
-    }
-
-    void locateMousePositionOnTerrain()
-    {
-        //shoot ray from camera to mouse click on terrain
-        //on terrain hit get that positon
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 1000)) {
-            Debug.Log(hit.collider.tag);
-            if (hit.collider.tag == "Floor") {
-            //fix y to the players position so it can't walk "up"
-            targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            }
-        }
-    }
-
-    void moveToPosition() 
-    {
-        //rotatePlayer();
         movePlayerTowardsPosition();
     }
 
+    private void mouseClick(CallbackContext context) {
+        //find position clicked on screen
+        Vector2 mousePosition = mouseInput.Mouse.MousePosition.ReadValue<Vector2>();
+        //add z to mousePosition (need to add camera distance);
+        Vector3 zPosition = new Vector3(mousePosition.x,mousePosition.y,Mathf.Abs(Camera.main.transform.position.z));
+        //translate position on screen to gameworld position
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(zPosition);
+        //translate worldPosition to gridPosition
+        Vector3Int gridPosition = map.WorldToCell(worldPosition);
+        //check if clicked position is on a tile
+        if (map.HasTile(gridPosition)) {
+            //remove depth movement
+            worldPosition.z = transform.position.z;
+            targetPosition = worldPosition;
+        }
+        rotatePlayer();
+    }
+
     void rotatePlayer() {
-        //rotate player into the target location
-        Quaternion playerRotation = Quaternion.LookRotation(targetPosition-transform.position);
-        playerRotation.x = 0f;
-        playerRotation.z = 0f;
-        transform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, Time.deltaTime * 10);
+        Vector2 direction = targetPosition - transform.position;
+        Vector3 topFromPlayer = new Vector3(transform.position.x, transform.position.y + 1,transform.position.z);
+        Vector2 rightDirection = topFromPlayer - transform.position;
+        float angle = Vector2.Angle(rightDirection, direction);
+        if (targetPosition.x < transform.position.x) {
+            angle *= -1;
+        }
+        transform.rotation = Quaternion.Euler(0,angle,0);
     }
 
     void movePlayerTowardsPosition() {
-        Vector3 offset = targetPosition - transform.position;
-        if (offset.magnitude > 0.7f) {
-        rotatePlayer();
-        //If we're further away than .7 unit, move towards the target.
-        //The minimum allowable tolerance varies with the speed of the object and the framerate. 
-        // 2 * tolerance must be >= moveSpeed / framerate or the object will jump right over the stop.
-        offset = offset.normalized * movementSpeed * Time.smoothDeltaTime;
-        //normalize it and account for movement speed.
-        //actually move the character.
-        characterController.Move(offset);
+        if (Vector3.Distance(transform.position, targetPosition) > 0.1f) {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.smoothDeltaTime);
         //play walking animation
         animator.SetBool("isWalking", true);
         } else {
